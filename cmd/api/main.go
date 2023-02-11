1ce36a538aa6c3e0eaab4ca7d76ae44ac4dd46d9
mickovi <m.oviedo@uni.pe>
2023-02-10 20:04:28 -0500

    mux router is replaced by httprouter

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -44,14 +44,10 @@ func main() {
 		logger: logger,
 	}
 
-	// Instance a new serveMux
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	// HTTP server configuration.
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
