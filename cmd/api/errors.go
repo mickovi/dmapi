0eb6e1ba580ea984314acf4ec0289f766a59fcaa
mickovi <m.oviedo@uni.pe>
2023-02-21 15:49:22 -0500

    Added editConflictResponse function.

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -61,3 +61,10 @@ func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Reques
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
+
+// editConflictResponse() helper sends a 409 Conflict response, along with a plain-English error message 
+// that explains the problem to the client.
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+    message := "unable to update the record due to an edit conflict, please try again"
+    app.errorResponse(w, r, http.StatusConflict, message)
+}
