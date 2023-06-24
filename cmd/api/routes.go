c156e022c946e094da986341f2b3f89ceb3f2758
mickovi <m.oviedo@uni.pe>
2023-06-24 11:07:16 -0500

    routes created for the endpoints

---
+++ cmd/api/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	// Initialize a new httprouter router instance.
+	router := httprouter.New()
+
+	// Register the relevant methods, URL pattterns and handler functions for
+	// the endpoints.
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckhandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+
+	return router
+}
