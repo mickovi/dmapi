0fe4b064a8f3a714ce422a98edb2b4948b9a1474
mickovi <m.oviedo@uni.pe>
2023-06-26 15:15:58 -0500

    added routing errors

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,9 @@ func (app *application) routes() *httprouter.Router {
 	// Initialize a new httprouter router instance.
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// Register the relevant methods, URL pattterns and handler functions for
 	// the endpoints.
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckhandler)
