c5e24fa0f24d1d5d195c7076dd25b4e525cf3c28
mickovi <m.oviedo@uni.pe>
2023-06-26 15:23:00 -0500

    addded info about the application in the JSON response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // healthcheckhandler method writes a plain-text response with information about the
 // application status, operating enviroment and version.
 func (app *application) healthcheckhandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// Declare an envelope map containing the data for the response.
+	env := envelope{
+		"status":      "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
